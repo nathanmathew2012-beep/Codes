b0be77dd2201b0f567768a374df0bf27833b4008
nathanmathew2012-beep <nathanmathew2012@gmail.com>
2026-06-22 16:38:58 +0300

    Lesson_1

---
+++ Lesson1/Act_1.py
@@ -0,0 +1,16 @@
+print("How are you") # in built function
+print("product is :",3*5,"sum is", 4+4 )
+
+
+
+print("Have a nice day\n")
+
+print("@3@")
+print(3+5-9*5)
+x="name" 
+print(x)
+print("what are you doing", end="?")
+
+print("nothing")
+print("Okay then,Bye", end="\n")
+print("Bye")
